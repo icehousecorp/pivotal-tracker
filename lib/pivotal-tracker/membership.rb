d34b97b6d8e8e550cb1cd9bbfc42fa1744ecda87
Doni Hanafi <doni@icehousecorp.com>
2013-03-01 10:05:10 +0700

    Add membership feature

--- lib/pivotal-tracker/membership.rb
+++ lib/pivotal-tracker/membership.rb
@@ -16,5 +16,37 @@ module PivotalTracker
     element :email, String, :deep => true
     element :initials, String, :deep => true
 
+    def initialize(attributes={})
+      update_attributes(attributes)
+    end
+
+    def assign(project, options={})
+      puts self.to_xml
+      response = Client.connection["/projects/#{project.id}/memberships/"].post(self.to_xml, :content_type => 'application/xml')
+      membership = Membership.parse(response)
+      return membership
+    end
+
+    protected
+
+      def to_xml
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml.membership {
+            xml.role "#{role}"
+            xml.person {
+              xml.name "#{name}"
+              xml.email "#{email}"
+              xml.initials "#{initials}" unless initials.nil?
+            }
+          }
+        end
+        return builder.to_xml
+      end
+
+      def update_attributes(attrs)
+        attrs.each do |key, value|
+          self.send("#{key}=", value.is_a?(Array) ? value.join(',') : value )
+        end
+      end
   end
 end
